81e789ea54641139a97d17b80500ca20852fdaf5
JENNIFER OH <ohjennifer.90@gmail.com>
2019-03-12 10:12:08 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -8,17 +8,7 @@ def turn_count(board)
   counter
 end
 
-def turn_count(board)
-  counter = 0
 
-  board.each do |position|
-    if position == "X" || position == "O"
-    counter += 1
-  end
-end
-counter
-
-end
 
 
 
